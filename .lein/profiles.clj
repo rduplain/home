9f8421f77fd1f68c5c426ef0d03deec5ac810263
Ron DuPlain <ron.duplain@gmail.com>
2016-05-19 16:20:30 -0400

    clojure: Configure lein.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,14 @@
 {:user
- {:plugins 
+ {:plugins
   [[jonase/eastwood "0.2.3"]
-   [lein-try "0.4.3"]]}}
+   [lein-ancient "0.6.10"]
+   [lein-create-template "0.2.0"]
+   [lein-maven-s3-wagon "0.2.5"]
+   [lein-midje "3.2"]
+   [lein-ring "0.9.7"]
+   [lein-try "0.4.3"]]}
+ :auth
+ {:repository-auth
+  {#"s3(p)?:\/\/rk-maven"
+   {:username :env/aws_access_key_id
+    :passphrase :env/aws_secret_key}}}}
