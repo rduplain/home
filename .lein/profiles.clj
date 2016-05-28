31a2974ffe21037bf84d2a31a0ca0fcc1cdf9ca8
Ron DuPlain <ron.duplain@gmail.com>
2016-05-28 13:16:27 -0400

    clojure: Only load CIDER with `lein repl`.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,13 +1,13 @@
 {:user
  {:plugins
-  [[cider/cider-nrepl "0.12.0"]
-   [jonase/eastwood "0.2.3"]
+  [[jonase/eastwood "0.2.3"]
    [lein-ancient "0.6.10"]
    [lein-create-template "0.2.0"]
    [lein-maven-s3-wagon "0.2.5"]
    [lein-midje "3.2"]
    [lein-ring "0.9.7"]
    [lein-try "0.4.3"]]}
+ :repl {:plugins [[cider/cider-nrepl "0.12.0"]]}
  :auth
  {:repository-auth
   {#"s3(p)?:\/\/rk-maven"
