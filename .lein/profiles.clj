ed10dbe03d5758c00ddd5128b00f09f7357c633a
Ron DuPlain <ron.duplain@gmail.com>
2016-05-28 13:12:31 -0400

    clojure: Connect lein and CIDER.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,7 @@
 {:user
  {:plugins
-  [[jonase/eastwood "0.2.3"]
+  [[cider/cider-nrepl "0.12.0"]
+   [jonase/eastwood "0.2.3"]
    [lein-ancient "0.6.10"]
    [lein-create-template "0.2.0"]
    [lein-maven-s3-wagon "0.2.5"]
