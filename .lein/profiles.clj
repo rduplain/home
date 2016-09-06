122c98bde38b2c724c29105ea2e515521b9501b1
Ron DuPlain <ron.duplain@gmail.com>
2016-09-06 10:19:30 -0400

    clojure: Upgrade profile dependencies.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,17 +1,17 @@
 {:user
  {:plugins
-  [[com.jakemccrary/lein-test-refresh "0.16.0"]
+  [[com.jakemccrary/lein-test-refresh "0.17.0"]
    [jonase/eastwood "0.2.3"]
    [lein-ancient "0.6.10"]
    [lein-create-template "0.2.0"]
    [lein-maven-s3-wagon "0.2.5"]
-   [lein-midje "3.2"]
+   [lein-midje "3.2.1"]
    [lein-ring "0.9.7"]
    [lein-try "0.4.3"]]
   :repl-options {:timeout 300000}
   :dependencies [[slamhound "1.5.5"]]
   :aliases {"slamhound" ["run" "-m" "slam.hound"]}}
- :repl {:plugins [[cider/cider-nrepl "0.12.0"]]}
+ :repl {:plugins [[cider/cider-nrepl "0.13.0"]]}
  :auth
  {:repository-auth
   {#"s3(p)?:\/\/rk-maven"
