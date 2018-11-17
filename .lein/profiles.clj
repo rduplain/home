bbf83970c0e5eaf876d9a5bcc85bfa768cfb2d60
Ron DuPlain <ron.duplain@gmail.com>
2018-11-17 00:16:59 -0500

    clojure: Remove features which are no longer used.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,20 +1,10 @@
 {:user
  {:plugins
-  [[com.jakemccrary/lein-test-refresh "0.22.0"]
-   [lein-ancient "0.6.15"]
-   [lein-cloverage "1.0.10"]
+  [[lein-ancient "0.6.15"]
    [lein-create-template "0.2.0"]
    [lein-midje "3.2.1"]
    [lein-ring "0.12.4"]
    [lein-tach "1.0.0"]
-   [lein-try "0.4.3"]
-   [savagematt/lein-maven-s3-wagon "0.2.5"]]
-  :repl-options {:timeout 300000}
-  :dependencies [[slamhound "1.5.5"]]
-  :aliases {"slamhound" ["run" "-m" "slam.hound"]}}
- :repl {:plugins [[cider/cider-nrepl "0.17.0"]]}
- :auth
- {:repository-auth
-  {#"s3(p)?:\/\/rk-maven"
-   {:username :env/aws_access_key_id
-    :passphrase :env/aws_secret_access_key}}}}
+   [lein-try "0.4.3"]]
+  :repl-options {:timeout 300000}}
+ :repl {:plugins [[cider/cider-nrepl "0.17.0"]]}}
