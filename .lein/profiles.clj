2fbfcc66c909b3b9d162e1878099881bab46e1e5
Ron DuPlain <ron.duplain@gmail.com>
2018-11-19 21:18:06 -0500

    clojure: Remove Node.js REPL.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -5,7 +5,6 @@
    [lein-midje "3.2.1"]
    [lein-ring "0.12.4"]
    [lein-tach "1.0.0"]
-   [lein-try "0.4.3"]
-   [org.bodil/lein-noderepl "0.1.11"]]
+   [lein-try "0.4.3"]]
   :repl-options {:timeout 300000}}
  :repl {:plugins [[cider/cider-nrepl "0.17.0"]]}}
