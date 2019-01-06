b3b81f9ba10fb56a1300fb443614a908f865cd8a
Ron DuPlain <ron.duplain@gmail.com>
2019-01-06 07:45:36 -0500

    Wrap `git` & `tig` to read .homegit when $HOME.
    The .homegit recipe still applies: avoid hitting the $HOME git repository
    when git runs in a subdirectory of $HOME.

---
+++ bin/wrapper.bash
@@ -0,0 +1,49 @@
+# Build a wrapper executable.
+
+command_exists() {
+    # Check if command exists, looking for programs and bash functions/aliases.
+    #
+    # usage: command_exists COMMAND
+    #
+    # Exit status:
+    #     2 if no arguments are given
+    #     1 if given command is not found
+    #     0 otherwise
+
+    # Return now if there are no arguments.
+    [[ $# -eq 0 ]] && return 2
+
+    local command="$1"
+    shift
+
+    type -t "$command" >/dev/null 2>&1
+}
+
+set_aside_this_and_find_that() {
+    # Set aside directory $1 and search PATH for command $2.
+
+    if [ $# -ne 2 ]; then
+        echo "usage: set_aside_this_and_find_that DIRECTORY COMMAND" >&2
+        return 2
+    fi
+
+    local directory_aside="$1"
+    local command="$2"
+    shift 2
+
+    # Loop in a subshell to contain IFS and set changes.
+    (
+        IFS=:
+        set -o noglob
+
+        for path in $PATH; do
+            if [ "$path" = "$directory_aside" ]; then
+                continue
+            fi
+            if [ -x "$path/$command" ]; then
+                printf %s "$path/$command"
+                break
+            fi
+        done
+    )
+}
