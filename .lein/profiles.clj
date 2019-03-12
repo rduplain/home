4b52741dccb8ed85bf602ed84f99d66988f1f5d9
Ron DuPlain <ron.duplain@gmail.com>
2019-03-12 19:15:06 -0400

    clojure: Update dependencies.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -4,7 +4,7 @@
    [lein-create-template "0.2.0"]
    [lein-licenses "0.2.2"]
    [lein-midje "3.2.1"]
-   [lein-ring "0.12.4"]
+   [lein-ring "0.12.5"]
    [lein-tach "1.0.0"]
    [lein-try "0.4.3"]]}
- :repl {:plugins [[cider/cider-nrepl "0.20.0"]]}}
+ :repl {:plugins [[cider/cider-nrepl "0.21.1"]]}}
