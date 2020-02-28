76776fa9f96ca7557e8e5647d3a13638bee3c16e
Rom DuPlain <rom.duplain@gmail.com>
2020-02-28 14:13:59 -0800

    Only sync `homegit` if upstream updated.

--- .box/lib/box.bash
+++ .box/lib/box.bash
@@ -2,8 +2,21 @@
 #
 # Assume core.bash is sourced.
 
-home-rev() {
+home-ref() {
+    # Report git revision for given ref.
+
+    with_cd "$HOME" "$HOME"/bin/homegit rev-parse --short "$1"
+}
+
+home-head() {
     # Report $HOME git HEAD revision.
 
     with_cd "$HOME" "$HOME"/bin/homegit rev-parse --short HEAD
+}
+
+home-upstream() {
+    # Report upstream ref, e.g. 'origin/master', to stdout (without quotes).
+
+    with_cd "$HOME" "$HOME"/bin/homegit rev-parse \
+            --abbrev-ref --symbolic-full-name '@{upstream}' 2>/dev/null
 }
