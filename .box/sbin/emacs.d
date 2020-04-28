4b273a60fbd98069192cff0f153df7c1be558e7f
Rom DuPlain <rom.duplain@gmail.com>
2020-04-27 17:51:35 -0700

    Drop check for SSH agent keys on emacs.d transfer.

--- .box/sbin/emacs.d
+++ .box/sbin/emacs.d
@@ -1,18 +1,11 @@
 #!/usr/bin/env bash
 # Sync ~/.emacs.d files from upstream.
 
-source "$REQD_LIB"/ssh.bash
-
 check() {
     return 1
 }
 
 install() {
-    if ! ssh_agent_has_keys; then
-        reqd_error "no keys available via ssh-agent. skipping ..."
-        return
-    fi
-
     reqd_echo 'use default upstream with `emacs-sync` ...'
     "$HOME"/bin/emacs-sync get
 }
