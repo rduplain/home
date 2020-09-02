bad11ce2625eeb6a6c7df909bb19366441f55da1
Rom DuPlain <rom.duplain@gmail.com>
2020-09-02 11:00:19 -0700

    Install Conda for Python tools.

--- .box/lib/core.bash
+++ .box/lib/core.bash
@@ -1,5 +1,25 @@
 # core.bash - Core shell API and language improvements.
 
+contains() {
+    # Check whether first argument exists in remaining arguments.
+
+    local match
+    match="$1"
+    shift
+
+    case "$*" in *"$match"*) return 0;; esac; return 1
+}
+
+startswith() {
+    # Check whether first argument exists at the start of remaining arguments.
+
+    local substr
+    substr="$1"
+    shift
+
+    case "$*" in "$substr"*) return 0;; esac; return 1
+}
+
 with_cd() {
     # Run command within a directory change.
 
