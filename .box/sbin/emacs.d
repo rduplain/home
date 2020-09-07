693fa2a59dd3bd43c68a8b223eb4461754b557f2
Rom DuPlain <rom.duplain@gmail.com>
2020-09-07 01:47:27 -0700

    Assume primary development in emacs.d.

--- .box/sbin/emacs.d
+++ .box/sbin/emacs.d
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
-# Sync ~/.emacs.d files from upstream.
+# Sync ~/.emacs.d files with upstream.
 
 check() {
     return 1
 }
 
 install() {
-    reqd_echo 'use default upstream with `emacs-sync` ...'
-    "$HOME"/bin/emacs-sync get
+    # Assume host 'espresso' is primary development machine.
+    if [ "$(uname -n)" = "espresso" ]; then
+        reqd_echo 'put latest to default upstream with `emacs-sync` ...'
+        "$HOME"/bin/emacs-sync put
+    else
+        reqd_echo 'get latest from default upstream with `emacs-sync` ...'
+        "$HOME"/bin/emacs-sync get
+    fi
 }
 
 reqd_main "$@"
