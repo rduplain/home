b24ebb9aeef242d297abb8ba052601a57883bc24
Rom DuPlain <rom.duplain@gmail.com>
2020-11-30 11:30:35 -0800

    clojure: Update dependencies.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -4,6 +4,6 @@
    [lein-licenses "0.2.2"]
    [lein-midje "3.2.2"]
    [lein-ring "0.12.5"]
-   [lein-tach "1.0.0"]
+   [lein-tach "1.1.0"]
    [lein-try "0.4.3"]]}
- :repl {:plugins [[cider/cider-nrepl "0.25.4"]]}}
+ :repl {:plugins [[cider/cider-nrepl "0.25.5"]]}}
